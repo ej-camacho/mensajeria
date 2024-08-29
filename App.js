df04f397da0eec3334a59023d7f13963fb4c11d5
Erick Javier Camacho Villanueva <49619419+erick-camvill@users.noreply.github.com>
2024-08-29 15:48:41 -0600

    pequeño cambio en App.js

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-// import CreateUser from './CreateUser';
 import Auth from './Auth';
 
 export default function App() {
   return (
     <>
-      {/* <CreateUser /> */}
       <Auth />
       <StatusBar style="auto" />
     </>
